7b24f6f36f0d5672773c15111b2ba0addb62212b
yurrrri <leeyuri.andapp@gmail.com>
2021-03-12 05:11:08 +0900

    충돌 해결

--- app/src/main/java/makeus6/hackathon/homecafe/src/main/MainActivity.kt
+++ app/src/main/java/makeus6/hackathon/homecafe/src/main/MainActivity.kt
@@ -1,14 +1,10 @@
 package makeus6.hackathon.homecafe.src.main
 
-
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.kakao.sdk.common.util.Utility
-import makeus6.hackathon.homecafe.src.main.home.HomeFragment
-
-import makeus6.hackathon.homecafe.src.main.myPage.MyPageFragment
 import makeus6.hackathon.homecafe.R
 import makeus6.hackathon.homecafe.config.BaseActivity
 import makeus6.hackathon.homecafe.databinding.ActivityMainBinding
@@ -31,15 +27,13 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
                             .commitAllowingStateLoss()
                         return@OnNavigationItemSelectedListener true
                     }
-                    R.id.menu_main_btm_nav_my_page -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.main_frm, MyPageFragment())
-                            .commitAllowingStateLoss()
-                        return@OnNavigationItemSelectedListener true
+                    R.id.menu_main_btm_nav_plus->{
+                        startActivity(Intent(this, AddPhotoActivity::class.java))
                     }
                 }
                 false
             })
+
 
         val keyHash = Utility.getKeyHash(this)
         Log.d("Hash", keyHash)
--- app/src/main/java/makeus6/hackathon/homecafe/src/main/home/HomeFragment.kt
+++ app/src/main/java/makeus6/hackathon/homecafe/src/main/home/HomeFragment.kt
@@ -1,17 +1,10 @@
 package makeus6.hackathon.homecafe.src.main.home
 
-
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.softsquared.template.kotlin.config.BaseFragment
-import com.softsquared.template.kotlin.src.main.home.HomeFragmentView
-import com.softsquared.template.kotlin.src.main.home.HomeService
-import com.softsquared.template.kotlin.src.main.home.models.PostSignUpRequest
-import com.softsquared.template.kotlin.src.main.home.models.SignUpResponse
-import com.softsquared.template.kotlin.src.main.home.models.UserResponse
 import makeus6.hackathon.homecafe.R
+import makeus6.hackathon.homecafe.config.BaseFragment
 import makeus6.hackathon.homecafe.databinding.FragmentHomeBinding
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind, R.layout.fragment_home){
@@ -19,11 +12,8 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::bind
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
         binding.feed.layoutManager=
             LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
 
-
     }
-
 }
